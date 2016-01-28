84b20755f2ee8026c1ef0a48ecf25c5f8c592686
Samuel Attard <samuel.r.attard@gmail.com>
2016-01-28 23:04:29 +1300

    Implemented thunbbarButtons on windows

--- src/inject/GPMInject/playback/handler.js
+++ src/inject/GPMInject/playback/handler.js
@@ -1,10 +1,17 @@
 window.wait(() => {
-  window.GPMTheme.enable();
-  // window.GPMTheme.on('enable', () => {
-  //   Emitter.fireAtMain('theme', true);
-  // });
-  //
-  // window.GPMTheme.on('disable', () => {
-  //   Emitter.fireAtMain('theme', false);
-  // });
+  window.GPM.on('change:playback', (mode) => {
+    switch (mode) {
+      case window.GMusic.Playback.STOPPED:
+        Emitter.fire('playback:isStopped');
+        break;
+      case window.GMusic.Playback.PAUSED:
+        Emitter.fire('playback:isPaused');
+        break;
+      case window.GMusic.Playback.PLAYING:
+        Emitter.fire('playback:isPlaying');
+        break;
+      default:
+        break;
+    }
+  });
 });
--- src/main/features/win32/index.js
+++ src/main/features/win32/index.js
@@ -1,1 +1,2 @@
 import './notificationPolyfill';
+import './thumbbarButtons';
